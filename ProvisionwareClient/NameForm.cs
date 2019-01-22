2084a66e4cb6967e82555e80730b2c330fdf5a47
Gorillarock <jdsacharok@gmail.com>
2019-01-21 22:57:46 -0800

    Add files via upload
    Here is an up to date version of the Provisionware client and server.  They are both a part of the same solution on this end, so please ensure that this does not conflict when merging.  I worked exclusively in the Provisionware Client, and did extensive UI fabrication.  NameForm class created to deal with registering name with client session.  Client has a prototype catalog (populated with hard coding in the FormCli constructor).  Cart works, but needs a column to indicate quantity (on client side).  Submit does in fact transmit request to Server.

---
+++ ProvisionwareClient/NameForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using proCart;
+
+namespace ProvisionwareClient
+{
+    public partial class NameForm : Form
+    {
+        private Requestor requestor;
+
+        public NameForm()
+        {
+            InitializeComponent();
+            this.requestor = new Requestor("default", "user");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //click on the Submit Button
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FormCli session = new FormCli(requestor);
+            this.Hide();
+            session.FormClosed += (s, args) => this.Close();
+            session.Show();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //text box for first name
+        private void textBoxFirstName_TextChanged(object sender, EventArgs e)
+        {
+            this.requestor.setFirstName(textBoxFirstName.Text);
+        }
+
+        //text box for last name
+        private void textBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+            this.requestor.setLastName(textBoxLastName.Text);
+        }
+    }
+}
---
+++ ProvisionwareClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProvisionwareClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProvisionwareClient")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba16fe9c-e7da-4637-ae10-31a20e8d8320")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
